0784f4296007de255895b755f2505e3be664bebd
Marco Antonio Carrillo Esparza <marco.37099p@gmail.com>
2024-01-15 21:24:27 -0600

    mas colecciones

--- Colecciones/Program.cs
+++ Colecciones/Program.cs
@@ -111,3 +111,74 @@ Console.WriteLine($"Buscando la cancion El Drip: {ListaReproduccion.Contains("El
 //Acceder a la data utilizando los metodos Next y Previous
 Console.WriteLine($"la cancion que sigue de la primera cancion {primeraCancion.Next!.Value }");
 Console.WriteLine($"la cancion que sigue de la cancion anterior {ultimaCancion.Previous!.Value}");
+
+
+Console.WriteLine($"//////////////////////////////////////////////STACK//////////////////////////////////////");
+
+Stack<string> miStack = new Stack<string>();
+
+//  Insertar elementos al stack
+miStack.Push("uno");
+miStack.Push("dos");
+miStack.Push("tres");
+miStack.Push("cuatro");
+miStack.Push("cinco");
+miStack.Push("seis");
+
+//  Imprimir los elementos del stack
+foreach (var item in miStack)
+{
+    Console.WriteLine($"numero dentro del stack: {item}");
+}
+
+//  Buscar el primer elemento del stack
+string top = miStack.Peek();
+Console.WriteLine($"el primer elemento del stack es: {top}");
+
+// Eliminar un elemento del stack
+string elemento = miStack.Pop();
+Console.WriteLine($"Elemento eliminado: {elemento}");
+foreach (var item in miStack)
+{
+    Console.WriteLine($"numero dentro del stack: {item}");
+}
+
+//  Buscamos elementos con el contains()
+var elementoExiste = miStack.Contains("tres");
+Console.WriteLine($"Existe el elemento en el stack? {elementoExiste}");
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+Console.WriteLine($"///////////////////////////////QUEUE////////////////////////////////////");
+
+Queue<string> cola = new Queue<string>();
+
+//Agregar elementos al Q
+
+cola.Enqueue("uno");
+cola.Enqueue("dos");
+cola.Enqueue("tres");
+cola.Enqueue("cuatro");
+cola.Enqueue("cinco");
+cola.Enqueue("seis");
+
+// Mostrar la Q
+foreach (var item in cola)
+{
+    Console.WriteLine($"Elemento enla cola: {item}");
+}
+
+//  Buscar el primer elemento de la Q
+var miPrimerElemento = cola.Peek();
+Console.WriteLine($"Mi primer elemento en la Q: {miPrimerElemento}");
+
+//  Eliminar un elemento de la Q
+var elementoEliminadoDeLaQ = cola.Dequeue();
+Console.WriteLine($"Sel elimino el elemento: {elementoEliminadoDeLaQ}");
+foreach (var item in cola)
+{
+    Console.WriteLine($"Elemento enla cola: {item}");
+}
+
+//  Buscar un elemento con contains
+var existeElmentoEnLaQ = cola.Contains("dos");
+Console.WriteLine($"Existe el elemento en la Q?: {existeElmentoEnLaQ}");
